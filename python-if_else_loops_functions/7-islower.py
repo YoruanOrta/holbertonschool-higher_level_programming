e7a2f66035a59c00edcb802184310436f553e0c9
YoruanOrta <yoruanorta50@gmail.com>
2025-01-16 13:24:07 -0400

    Making the task 7

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+
 def islower(c):
-    """Check if a character is lowercase."""
-    return ord(c) >= 97 and ord(c) <= 122
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
