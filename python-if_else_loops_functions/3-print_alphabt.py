9f74489e9a5427abeaff59a41dc6e13fed3dc903
YoruanOrta <yoruanorta50@gmail.com>
2025-01-16 12:31:49 -0400

    Making the task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join("{:c}".format(c) for c in range(97, 123) if c != 101 and c != 113), end="")
+print("".join("{:c}".format(c) for c in range(97, 123)
+              if c != 101 and c != 113), end="")
