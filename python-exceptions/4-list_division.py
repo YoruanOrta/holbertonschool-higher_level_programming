3d7717d4847fac0b6cfefe06ede55641b0f4d87f
YoruanOrta <yoruanorta50@gmail.com>
2025-01-24 14:56:56 -0400

    Making the task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -12,5 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(0)
         except IndexError:
             print("out of range")
-            result.append(0)
+            result = 0
+	finally:
+	result.append(result)
     return result
