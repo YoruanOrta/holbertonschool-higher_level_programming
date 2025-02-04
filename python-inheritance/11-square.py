dcb2ca12d90cc459ee0cb712bb2facb996e68354
YoruanOrta <yoruanorta50@gmail.com>
2025-02-04 13:38:06 -0400

    fixed the output

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -12,3 +12,7 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """String representation of the square"""
+        return f"[Square] {self.__size}/{self.__size}"
