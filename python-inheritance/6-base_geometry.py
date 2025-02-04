7de4d4429bd0a148d71bd5f1b677a23244f1b9f4
YoruanOrta <yoruanorta50@gmail.com>
2025-02-04 13:01:26 -0400

    Add BaseGeometry class with area method and test implementation

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" BaseGeometry class """
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+    def area(self):
+        """ area method """
+        raise Exception("area() is not implemented")
