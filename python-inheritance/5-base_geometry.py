c1b6014657d35e907a673225f8fb585c38f9e2bf
YoruanOrta <yoruanorta50@gmail.com>
2025-02-04 12:51:01 -0400

    Remove area method placeholder from BaseGeometry class

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,6 +4,4 @@
 
 class BaseGeometry:
     """A class with public instance method area"""
-    def area(self):
-        """Raises an Exception with the message area() is not implemented"""
-        raise Exception("area() is not implemented")
+    pass
