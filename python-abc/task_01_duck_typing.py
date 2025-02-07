fd38baaa2c917dd79f7b84b03dded82646e978ba
YoruanOrta <yoruanorta50@gmail.com>
2025-02-07 14:35:28 -0400

    fixing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -22,9 +22,7 @@ class Circle(Shape):
         return 2 * pi * self.radius
 
     def __init__(self, radius: float):
-        if radius < 0:
-            raise ValueError("Radius must be non-negative")
-        self.radius = radius
+        self.radius = max(0, radius)
 
 class Rectangle(Shape):
     def __init__(self, width: float, height: float):
