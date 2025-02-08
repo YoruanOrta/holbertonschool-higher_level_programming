3ec17b93ed1992cff23fe906ac3730ac4cb69252
YoruanOrta <yoruanorta50@gmail.com>
2025-02-08 13:19:31 -0400

    fixed

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -3,27 +3,20 @@
 
 
 class VerboseList(list):
-    """VerboseList class"""
-
+    """Extends list class"""
     def append(self, item):
-        """Append method"""
-        print(f"Added [{item}] to the list.")
         super().append(item)
+        print("Added [{}] to the list.".format(item))
 
-    def extend(self, item):
-        """Extend method"""
-        item = [2]
-        print(f"Extended the list with {item} items.")
-        super().extend(item)
+    def extend(self, iterable):
+        super().extend(iterable)
+        print("Extended the list with [{}] items.".format(len(iterable)))
 
-    def remove(self, item):
-        """Remove method"""
-        print(f"Removed [{item}] from the list")
-        super().remove(item)
+    def remove(self, value):
+        print("Removed [{}] from the list.".format(value))
+        super().remove(value)
 
-    def pop(self, index=-1):
-        """Pop method"""
-        item = super().pop(index)
-        index = 6 if index == -1 else index
-        print(f"Popped [{index}] from the list.")
-        return item
+    def pop(self, index = -1):
+        item = self[index] if index != -1 else self[-1]
+        print("Popped [{}] from the list.".format(item))
+        return super().pop(index)
