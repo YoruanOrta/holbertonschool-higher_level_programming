cea95b88bcc70365fb7f6640c126a6c3b45abb2f
YoruanOrta <yoruanorta50@gmail.com>
2025-02-08 17:29:10 -0400

    Implement FlyingFish class with swim, fly, and habitat methods, inheriting from Fish and Bird classes

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Class FlyingFish"""
+
+
+class Fish:
+    """Class Fish"""
+    def swim(self):
+        """Method swim"""
+        print("The fish is swimming.")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The fish lives in the water.")
+
+
+class Bird:
+    """Class Bird"""
+    def fly(self):
+        """Method fly"""
+        print("The bird is flying.")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The bird lives in the sky.")
+
+
+class FlyingFish(Fish, Bird):
+    """Class FlyingFish"""
+    def fly(self):
+        """Method fly"""
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        """Method swim"""
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        """Method habitat"""
+        print("The flying fish lives both in the water and the sky!")
