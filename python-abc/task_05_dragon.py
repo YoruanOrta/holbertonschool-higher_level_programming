b81d0a8930a94dfcfbe9e1ef81d3063c68180f3c
YoruanOrta <yoruanorta50@gmail.com>
2025-02-08 17:35:04 -0400

    Implement Dragon class with swim, fly, and roar methods

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module for the Dragon class."""
+
+
+class Dragon:
+    """Class representing a dragon."""
+
+    def swim(self):
+        """Make the dragon swim."""
+        print("The creature swims!")
+
+    def fly(self):
+        """Make the dragon fly."""
+        print("The creature flies!")
+
+    def roar(self):
+        """Make the dragon roar."""
+        print("The dragon roars!")
