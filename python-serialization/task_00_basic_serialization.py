bf18b2c36f72c7c530b35e48cf9773a6cf95f8a6
YoruanOrta <yoruanorta50@gmail.com>
2025-02-11 12:43:49 -0400

    Implement basic serialization with JSON save and load functions

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Basic serialization """
+
+import json
+
+
+def serialize_and_save_to_file(data_to_serialize, filename='data.json'):
+    """ Serialize data_to_serialize into JSON and save it to 'data.json' """
+    pass
+
+
+def load_and_deserialize(filename='data.json'):
+    """ Load data from 'data.json' and deserialize it """
+    pass
