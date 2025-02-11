38e9a02794bd63f98d899605e2b079157d2b4cf5
YoruanOrta <yoruanorta50@gmail.com>
2025-02-11 14:54:50 -0400

    Implement JSON serialization and deserialization functions for file operations

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,11 +4,14 @@
 import json
 
 
-def serialize_and_save_to_file(data_to_serialize, filename='data.json'):
-    """ Serialize data_to_serialize into JSON and save it to 'data.json' """
-    pass
+def serialize_and_save_to_file(data, filename):
+    """ Serialize data to JSON and save it to a file """
+    with open(filename, 'w') as f:
+        json.dump(data, f)
 
 
-def load_and_deserialize(filename='data.json'):
-    """ Load data from 'data.json' and deserialize it """
-    pass
+def load_and_deserialize(filename):
+    """ Load and deserialize data from a file """
+    with open(filename, 'r') as f:
+        data = json.load(f)
+    return data
