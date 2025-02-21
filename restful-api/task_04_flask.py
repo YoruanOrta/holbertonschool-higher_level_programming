14fead79a7acf2ea35b3fe93ea0b4cfd8cc9c673
YoruanOrta <yoruanorta50@gmail.com>
2025-02-21 12:14:23 -0400

    Implement Flask API with user management and data retrieval endpoints

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Task 04 - Flask """
+
+from flask import Flask, jsonify
+from flask import request
+app = Flask(__name__)
+
+users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"}}
+@app.route('/')
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route('/data')
+def get_usernames():
+    usernames = list(users.keys())
+    return jsonify(usernames)
+@app.route('/status')
+def status():
+    return "OK"
+
+@app.route('/users/<username>')
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    data = request.get_json()
+    username = data.get('username')
+    if not username or username in users:
+        return jsonify({"error": "Invalid or duplicate username"}), 400
+
+    users[username] = {
+        "name": data.get('name'),
+        "age": data.get('age'),
+        "city": data.get('city')
+    }
+    return jsonify({"message": "User added", "user": users[username]}), 201
+if __name__ == "__main__": app.run()
