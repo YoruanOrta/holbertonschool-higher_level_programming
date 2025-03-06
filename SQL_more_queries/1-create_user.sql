b6a1342a26dbc1a9ece421db2b931b03b6a6bafc
YoruanOrta <yoruanorta50@gmail.com>
2025-03-06 12:50:19 -0400

    fixed

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
+-- Path: SQL_more_queries/1-create_user.sql
 CREATE USER
 IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
--- Path: SQL_more_queries/1-create_user.sql
