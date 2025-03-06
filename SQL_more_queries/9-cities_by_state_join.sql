bcccd95aec6f5c389b037cef04808b9a2c236cde
YoruanOrta <yoruanorta50@gmail.com>
2025-03-06 13:59:40 -0400

    add SQL script to list all cities with their corresponding state names

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- List all cities with their corresponding state name
+SELECT cities.id, cities.name, 
+        (SELECT name FROM states WHERE states.id = cities.state_id) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
