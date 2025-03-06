73c009966142d2abd366a76a99b2b505e26f0819
YoruanOrta <yoruanorta50@gmail.com>
2025-03-06 12:34:59 -0400

    add SQL script to create a new user and grant privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+-- Path: SQL_more_queries/1-create_user.sql
