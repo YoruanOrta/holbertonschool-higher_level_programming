611948c8a48c1325ba0d448c92a042ea3019255b
YoruanOrta <yoruanorta50@gmail.com>
2025-03-06 14:27:31 -0400

    add SQL script to count TV shows without an assigned genre

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+-- Count the number of TV shows that do not have a genre assigned to them.
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
