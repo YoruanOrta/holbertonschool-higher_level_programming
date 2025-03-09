eed0a79e41e770331f5702f92b0f83d6a28fb1aa
YoruanOrta <yoruanorta50@gmail.com>
2025-03-09 16:05:10 -0400

    refactor: update state filtering script to use command-line arguments and improve SQL query handling

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
-""" Module that lists all states from the database hbtn_0e_0_usa
-    using the module MySQLdb and the command execute """
+""" Filter states by user input """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    # Connecting to a MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
-    cursor = db.cursor()
+    # Get command-line arguments
+    username, password, database, state_name = sys.argv[1:5]
 
-    # Executing the Query with parameterized input to prevent SQL injection
-    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (argv[4],))
+    # Connect to MySQL database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-    # Obtaining the Query results
-    query_rows = cursor.fetchall()
-    for row in query_rows:
+    # Create a cursor object to execute queries
+    cur = db.cursor()
+
+    # Execute the query with format (unsafe, but per requirements)
+    query = (
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+        .format(state_name)
+    )
+    cur.execute(query)
+
+    # Fetch and print results
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
 
-    # Closing cursor and database
-    cursor.close()
+    # Close the cursor and connection
+    cur.close()
     db.close()
