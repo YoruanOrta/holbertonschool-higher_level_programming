07341577b165f1eb68ddbb3ac1138169bf81f4be
YoruanOrta <yoruanorta50@gmail.com>
2025-03-09 17:13:02 -0400

    fix

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
-"""Prints the State object with the given name from the database."""
+"""
+prints the State object with the name passed as
+argument from the database hbtn_0e_6_usa
+"""
 
 import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print(
-            "Usage: ./10-model_state_my_get.py "
-            "mysql_username mysql_password database_name state_name"
-        )
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
     engine = create_engine(
-        f'mysql+mysqldb://{mysql_username}:{mysql_password}'
-        f'@localhost/{database_name}',
-        pool_pre_ping=True
-    )
-
+        'mysql+mysqldb://{}:{}@localhost:3306/{}?charset=utf8'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    state_name = sys.argv[4].capitalize()
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    state = session.query(State).filter(State.name == state_name).first()
-
-    if state:
-        print(state.id)
-    else:
+    found = False
+    for state in session.query(State).order_by(State.id).all():
+        if state_name == state.name:
+            print("{}".format(state.id))
+            found = True
+    if not found:
         print("Not found")
-
     session.close()
